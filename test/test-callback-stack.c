84ebdf842922102c53a970d72f20cc811e1dbe54
Ryan Dahl <ry@tinyclouds.org>
2011-04-15 10:27:09 -0700

    style fixes to close_cb_stack

--- test/test-callback-stack.c
+++ test/test-callback-stack.c
@@ -1,6 +1,9 @@
 #include "../oio.h"
 #include "test.h"
 
+/* TODO: Add explanation of why we want on_close to be called from fresh
+ * stack.
+ */
 
 int nested = 0;
 int close_cb_called = 0;
@@ -18,19 +21,22 @@ TEST_IMPL(close_cb_stack) {
 
   oio_init();
 
-  if (oio_tcp_handle_init(&handle, &close_cb, NULL))
+  if (oio_tcp_handle_init(&handle, &close_cb, NULL)) {
     FATAL(oio_tcp_handle_init failed)
+  }
 
   nested++;
 
-  if (oio_close(&handle))
+  if (oio_close(&handle)) {
     FATAL(oio_close failed)
+  }
 
   nested--;
 
   oio_run();
 
-  ASSERT(close_cb_called && "oio_close_cb must be called exactly once")
+  ASSERT(nested == 0)
+  ASSERT(close_cb_called == 1 && "oio_close_cb must be called exactly once")
 
   return 0;
 }
