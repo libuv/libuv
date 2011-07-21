186abb00bc46fedae47e5508d8008a8e354b676f
Igor Zinkovsky <igorzi@microsoft.com>
2011-07-21 10:38:05 -0700

    windows: return UV_EACCESS for invalid pipe name

--- src/win/pipe.c
+++ src/win/pipe.c
@@ -152,7 +152,7 @@ int uv_pipe_bind(uv_pipe_t* handle, const char* name) {
       handle->error = LOOP->last_error;
       handle->flags |= UV_HANDLE_BIND_ERROR;
     } else if (errno == ERROR_PATH_NOT_FOUND || errno == ERROR_INVALID_NAME) {
-      uv_set_error(UV_EADDRNOTAVAIL, errno);
+      uv_set_error(UV_EACCESS, errno);
     } else {
       uv_set_sys_error(errno);
     }
