92a19a19dd87f7dfeaf7aaf893099d0afd4a182f
Ben Noordhuis <info@bnoordhuis.nl>
2012-12-28 15:10:04 +0100

    unix: ensure done_cb gets called after uv_cancel()
    Wake up the event loop with uv_async_send() when a request is cancelled.
    Ensures the done_cb is run on the next tick of the event loop.
    
    Not sending a wakeup signal results in the done_cb not getting called until
    another request completes, which may be a long time coming when it's the only
    request in the queue or when other requests are executing long-running jobs.
    
    Fixes #669.

--- src/unix/threadpool.c
+++ src/unix/threadpool.c
@@ -158,6 +158,7 @@ int uv__work_cancel(uv_loop_t* loop, uv_req_t* req, struct uv__work* w) {
   w->work = uv__cancelled;
   uv_mutex_lock(&loop->wq_mutex);
   ngx_queue_insert_tail(&loop->wq, &w->wq);
+  uv_async_send(&loop->wq_async);
   uv_mutex_unlock(&loop->wq_mutex);
 
   return 0;
