libuv/libuv

commit 3fdd2a1128df16df2e196aa7ff6145d31c942171

uv: support android libuv standalone build
Tested most of my compilation in the previous patch on NodeJS
and extracted the patches from there. This patch ensures libuv
will be capable of building standalone as well, both with gyp
and Makefiles.

Build documentation was added to the README.md file.

Some tests are failing, and I have not heavily investigated
the reasons. The failures are generally on errors, and are
likely related to differences between fully POSIX-compatible
systems and android.

common.gypi
@@ -8,6 +8,13 @@
     'msvs_multi_core_compile': '0',  # we do enable multicore compiles, but not using the V8 way
     'gcc_version%': 'unknown',
     'clang%': 0,
+
+    'conditions': [
+      # Workaround for the legacy handling of android in gyp
+      ['android_build == 1', {
+        'OS': 'android',
+      }],
+    ],
   },
 
   'target_defaults': {
@@ -148,7 +155,8 @@
           [ 'OS=="solaris"', {
             'cflags': [ '-pthreads' ],
             'ldflags': [ '-pthreads' ],
-          }, {
+          }],
+          [ 'OS not in "android solaris"', {
             'cflags': [ '-pthread' ],
             'ldflags': [ '-pthread' ],
           }],

android-configure
@@ -1,0 +1,20 @@
+#!/bin/bash
+
+export TOOLCHAIN=$PWD/android-toolchain
+mkdir -p $TOOLCHAIN
+$1/build/tools/make-standalone-toolchain.sh \
+    --toolchain=arm-linux-androideabi-4.7 \
+    --arch=arm \
+    --install-dir=$TOOLCHAIN \
+    --platform=android-9
+export PATH=$TOOLCHAIN/bin:$PATH
+export AR=arm-linux-androideabi-ar
+export CC=arm-linux-androideabi-gcc
+export CXX=arm-linux-androideabi-g++
+export LINK=arm-linux-androideabi-g++
+export PLATFORM=android
+
+if [ $2 -a $2 == 'gyp' ]
+  then
+    ./gyp_uv -Dandroid_build=1 -Dtarget_arch=arm
+fi

config-unix.mk
@@ -19,7 +19,7 @@
 # IN THE SOFTWARE.
 
 E=
-CFLAGS += -g -Wall -Wextra -Wno-unused-parameter -std=c89
+CFLAGS += -g -Wall -Wextra -Wno-unused-parameter
 CPPFLAGS += -I$(SRCDIR)/src
 LDFLAGS=-lm
 
@@ -104,6 +104,18 @@ OBJS += src/unix/linux-core.o \
         src/unix/proctitle.o
 endif
 
+ifeq (android,$(PLATFORM))
+CFLAGS += -D_GNU_SOURCE
+LDFLAGS+=-ldl -lrt
+RUNNER_CFLAGS += -D_GNU_SOURCE
+OBJS += src/unix/linux-core.o \
+        src/unix/linux-inotify.o \
+        src/unix/linux-syscalls.o \
+        src/unix/proctitle.o
+else
+CFLAGS += -std=c89
+endif
+
 ifeq (freebsd,$(PLATFORM))
 HAVE_DTRACE=1
 LDFLAGS+=-lkvm
@@ -132,7 +144,9 @@ endif
 ifeq (sunos,$(PLATFORM))
 RUNNER_LDFLAGS += -pthreads
 else
+ifneq (android, $(PLATFORM))
 RUNNER_LDFLAGS += -pthread
+endif
 endif
 
 ifeq ($(HAVE_DTRACE), 1)

include/uv-private/uv-unix.h
@@ -37,7 +37,7 @@
 
 #include <semaphore.h>
 #include <pthread.h>
-#ifdef ANDROID
+#ifdef __ANDROID__
 #include "pthread-fixes.h"
 #endif
 #include <signal.h>

src/unix/linux-core.c
@@ -36,7 +36,7 @@
 #include <fcntl.h>
 #include <time.h>
 
-#ifndef ANDROID
+#ifndef __ANDROID__
 #define HAVE_IFADDRS_H 1
 #endif
 

src/unix/thread.c
@@ -281,7 +281,7 @@ int uv_cond_init(uv_cond_t* cond) {
   if (pthread_condattr_init(&attr))
     return -1;
 
-#if !defined(ANDROID)
+#if !defined(__ANDROID__)
   if (pthread_condattr_setclock(&attr, CLOCK_MONOTONIC))
     goto error2;
 #endif
@@ -336,7 +336,7 @@ int uv_cond_timedwait(uv_cond_t* cond, uv_mutex_t* mutex, uint64_t timeout) {
   timeout += uv__hrtime();
   ts.tv_sec = timeout / NANOSEC;
   ts.tv_nsec = timeout % NANOSEC;
-#if defined(ANDROID)
+#if defined(__ANDROID__)
   /*
    * The bionic pthread implementation doesn't support CLOCK_MONOTONIC,
    * but has this alternative function instead.
@@ -344,7 +344,7 @@ int uv_cond_timedwait(uv_cond_t* cond, uv_mutex_t* mutex, uint64_t timeout) {
   r = pthread_cond_timedwait_monotonic_np(cond, mutex, &ts);
 #else
   r = pthread_cond_timedwait(cond, mutex, &ts);
-#endif /* ANDROID */
+#endif /* __ANDROID__ */
 #endif
 
 

commit eb1f60c022ad651833913d2825ecced398993a3f

build: fix build breakage from 3fdd2a1
Make sure the android_build variable is defined.

common.gypi
@@ -1,5 +1,6 @@
 {
   'variables': {
+    'android_build%': 0,
     'visibility%': 'hidden',         # V8's visibility setting
     'target_arch%': 'ia32',          # set v8's target architecture
     'host_arch%': 'ia32',            # set v8's host architecture
@@ -8,13 +9,6 @@
     'msvs_multi_core_compile': '0',  # we do enable multicore compiles, but not using the V8 way
     'gcc_version%': 'unknown',
     'clang%': 0,
-
-    'conditions': [
-      # Workaround for the legacy handling of android in gyp
-      ['android_build == 1', {
-        'OS': 'android',
-      }],
-    ],
   },
 
   'target_defaults': {
@@ -136,7 +130,7 @@
           }]
         ]
       }],
-      ['OS in "android freebsd linux openbsd solaris"', {
+      ['OS in "freebsd linux openbsd solaris" or android_build == 1', {
         'cflags': [ '-Wall' ],
         'cflags_cc': [ '-fno-rtti', '-fno-exceptions' ],
         'target_conditions': [
@@ -156,7 +150,7 @@
             'cflags': [ '-pthreads' ],
             'ldflags': [ '-pthreads' ],
           }],
-          [ 'OS not in "android solaris"', {
+          [ 'OS != "solaris" and android_build == 0', {
             'cflags': [ '-pthread' ],
             'ldflags': [ '-pthread' ],
           }],

uv.gyp
@@ -1,5 +1,6 @@
 {
   'variables': {
+    'android_build%': 0,
     'uv_use_dtrace%': 'false',
     # uv_parent_path is the relative path to libuv in the parent project
     # this is only relevant when dtrace is enabled and libuv is a child project
@@ -21,7 +22,7 @@
           ['OS=="solaris"', {
             'cflags': [ '-pthreads' ],
           }],
-          ['OS not in "android solaris"', {
+          ['OS != "solaris" and android_build == 0', {
             'cflags': [ '-pthread' ],
           }],
         ],
@@ -161,7 +162,7 @@
               ['OS=="solaris"', {
                 'ldflags': [ '-pthreads' ],
               }],
-              ['OS not in "android solaris"', {
+              ['OS != "solaris" and android_build == 0', {
                 'ldflags': [ '-pthread' ],
               }],
             ],
@@ -179,7 +180,7 @@
             }],
           ],
         }],
-        [ 'OS in "android linux mac"', {
+        [ 'OS in "linux mac" or android_build == 1', {
           'sources': [ 'src/unix/proctitle.c' ],
         }],
         [ 'OS=="mac"', {
@@ -215,7 +216,7 @@
             'libraries': [ '-ldl', '-lrt' ],
           },
         }],
-        [ 'OS=="android"', {
+        [ 'android_build == 1', {
           'sources': [
             'src/unix/linux-core.c',
             'src/unix/linux-inotify.c',