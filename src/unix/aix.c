libuv/libuv

commit 3ee4d3f183331a123ce35edd0d32268a2bb22aa5

unix, windows: return error codes directly
This commit changes the libuv API to return error codes directly rather
than storing them in a loop-global field.

A code snippet like this one:

    if (uv_foo(loop) < 0) {
      uv_err_t err = uv_last_error(loop);
      fprintf(stderr, "%s\n", uv_strerror(err));
    }

Should be rewritten like this:

    int err = uv_foo(loop);
    if (err < 0)
      fprintf(stderr, "%s\n", uv_strerror(err));

The rationale for this change is that it should make creating bindings
for other languages a lot easier: dealing with struct return values is
painful with most FFIs and often downright buggy.

src/unix/aix.c
@@ -134,8 +134,7 @@ int uv_fs_event_init(uv_loop_t* loop,
                      uv_fs_event_cb cb,
                      int flags) {
   loop->counters.fs_event_init++;
-  uv__set_sys_error(loop, ENOSYS);
-  return -1;
+  return -ENOSYS;
 }
 
 
@@ -149,45 +148,44 @@ char** uv_setup_args(int argc, char** argv) {
 }
 
 
-uv_err_t uv_set_process_title(const char* title) {
-  return uv_ok_;
+int uv_set_process_title(const char* title) {
+  return 0;
 }
 
 
-uv_err_t uv_get_process_title(char* buffer, size_t size) {
+int uv_get_process_title(char* buffer, size_t size) {
   if (size > 0) {
     buffer[0] = '\0';
   }
-  return uv_ok_;
+  return 0;
 }
 
 
-uv_err_t uv_resident_set_memory(size_t* rss) {
+int uv_resident_set_memory(size_t* rss) {
   char pp[64];
   psinfo_t psinfo;
-  uv_err_t err;
+  int err;
   int fd;
 
   (void) snprintf(pp, sizeof(pp), "/proc/%lu/psinfo", (unsigned long) getpid());
 
   fd = open(pp, O_RDONLY);
   if (fd == -1)
-    return uv__new_sys_error(errno);
+    return -errno;
 
-  err = uv_ok_;
-
-  if (read(fd, &psinfo, sizeof(psinfo)) == sizeof(psinfo))
+  /* FIXME(bnoordhuis) Handle EINTR. */
+  err = -EINVAL;
+  if (read(fd, &psinfo, sizeof(psinfo)) == sizeof(psinfo)) {
     *rss = (size_t)psinfo.pr_rssize * 1024;
-  else
-    err = uv__new_sys_error(EINVAL);
-
+    err = 0;
+  }
   close(fd);
 
   return err;
 }
 
 
-uv_err_t uv_uptime(double* uptime) {
+int uv_uptime(double* uptime) {
   struct utmp *utmp_buf;
   size_t entries = 0;
   time_t boot_time;
@@ -206,14 +204,14 @@ uv_err_t uv_uptime(double* uptime) {
   endutent();
 
   if (boot_time == 0)
-    return uv__new_artificial_error(UV_ENOSYS);
+    return -ENOSYS;
 
   *uptime = time(NULL) - boot_time;
-  return uv_ok_;
+  return 0;
 }
 
 
-uv_err_t uv_cpu_info(uv_cpu_info_t** cpu_infos, int* count) {
+int uv_cpu_info(uv_cpu_info_t** cpu_infos, int* count) {
   uv_cpu_info_t* cpu_info;
   perfstat_cpu_total_t ps_total;
   perfstat_cpu_t* ps_cpus;
@@ -222,30 +220,30 @@ uv_err_t uv_cpu_info(uv_cpu_info_t** cpu_infos, int* count) {
 
   result = perfstat_cpu_total(NULL, &ps_total, sizeof(ps_total), 1);
   if (result == -1) {
-    return uv__new_artificial_error(UV_ENOSYS);
+    return -ENOSYS;
   }
 
   ncpus = result = perfstat_cpu(NULL, NULL, sizeof(perfstat_cpu_t), 0);
   if (result == -1) {
-    return uv__new_artificial_error(UV_ENOSYS);
+    return -ENOSYS;
   }
 
   ps_cpus = (perfstat_cpu_t*) malloc(ncpus * sizeof(perfstat_cpu_t));
   if (!ps_cpus) {
-    return uv__new_artificial_error(UV_ENOMEM);
+    return -ENOMEM;
   }
 
   strcpy(cpu_id.name, FIRST_CPU);
   result = perfstat_cpu(&cpu_id, ps_cpus, sizeof(perfstat_cpu_t), ncpus);
   if (result == -1) {
     free(ps_cpus);
-    return uv__new_artificial_error(UV_ENOSYS);
+    return -ENOSYS;
   }
 
   *cpu_infos = (uv_cpu_info_t*) malloc(ncpus * sizeof(uv_cpu_info_t));
   if (!*cpu_infos) {
     free(ps_cpus);
-    return uv__new_artificial_error(UV_ENOMEM);
+    return -ENOMEM;
   }
 
   *count = ncpus;
@@ -264,7 +262,7 @@ uv_err_t uv_cpu_info(uv_cpu_info_t** cpu_infos, int* count) {
   }
 
   free(ps_cpus);
-  return uv_ok_;
+  return 0;
 }
 
 
@@ -279,7 +277,7 @@ void uv_free_cpu_info(uv_cpu_info_t* cpu_infos, int count) {
 }
 
 
-uv_err_t uv_interface_addresses(uv_interface_address_t** addresses,
+int uv_interface_addresses(uv_interface_address_t** addresses,
   int* count) {
   uv_interface_address_t* address;
   int sockfd, size = 1;
@@ -289,19 +287,19 @@ uv_err_t uv_interface_addresses(uv_interface_address_t** addresses,
   *count = 0;
 
   if (0 > (sockfd = socket(AF_INET, SOCK_DGRAM, IPPROTO_IP))) {
-    return uv__new_artificial_error(UV_ENOSYS);
+    return -ENOSYS;
   }
 
   if (ioctl(sockfd, SIOCGSIZIFCONF, &size) == -1) {
     close(sockfd);
-    return uv__new_artificial_error(UV_ENOSYS);
+    return -ENOSYS;
   }
 
   ifc.ifc_req = (struct ifreq*)malloc(size);
   ifc.ifc_len = size;
   if (ioctl(sockfd, SIOCGIFCONF, &ifc) == -1) {
     close(sockfd);
-    return uv__new_artificial_error(UV_ENOSYS);
+    return -ENOSYS;
   }
 
 #define ADDR_SIZE(p) MAX((p).sa_len, sizeof(p))
@@ -334,7 +332,7 @@ uv_err_t uv_interface_addresses(uv_interface_address_t** addresses,
     malloc(*count * sizeof(uv_interface_address_t));
   if (!(*addresses)) {
     close(sockfd);
-    return uv__new_artificial_error(UV_ENOMEM);
+    return -ENOMEM;
   }
   address = *addresses;
 
@@ -377,7 +375,7 @@ uv_err_t uv_interface_addresses(uv_interface_address_t** addresses,
 #undef ADDR_SIZE
 
   close(sockfd);
-  return uv_ok_;
+  return 0;
 }
 
 

commit 4bdb7d837ad99e59974274bb87fc10a35cbc1bf1

aix: fix build breakage introduced in 3ee4d3f
Possibly, anyway - this change is untested. Without it, libuv
definitely won't build on AIX though.

src/unix/aix.c
@@ -318,7 +318,7 @@ int uv_interface_addresses(uv_interface_address_t** addresses,
     memcpy(flg.ifr_name, p->ifr_name, sizeof(flg.ifr_name));
     if (ioctl(sockfd, SIOCGIFFLAGS, &flg) == -1) {
       close(sockfd);
-      return uv__new_artificial_error(UV_ENOSYS);
+      return -ENOSYS;
     }
 
     if (!(flg.ifr_flags & IFF_UP && flg.ifr_flags & IFF_RUNNING))
@@ -349,7 +349,7 @@ int uv_interface_addresses(uv_interface_address_t** addresses,
     memcpy(flg.ifr_name, p->ifr_name, sizeof(flg.ifr_name));
     if (ioctl(sockfd, SIOCGIFFLAGS, &flg) == -1) {
       close(sockfd);
-      return uv__new_artificial_error(UV_ENOSYS);
+      return -ENOSYS;
     }
 
     if (!(flg.ifr_flags & IFF_UP && flg.ifr_flags & IFF_RUNNING))