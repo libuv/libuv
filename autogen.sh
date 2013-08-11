0e4fa705f0e9f40d9d58e03e381f2605b7d7c223
Ben Noordhuis <info@bnoordhuis.nl>
2013-08-11 12:12:58 +0200

    build: fix string comparisons in autogen.sh
    Compare strings with '=', not '=='.  The second form doesn't work with
    all shells.
    
    Fixes #876.

--- autogen.sh
+++ autogen.sh
@@ -14,7 +14,7 @@
 # ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
 # OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 
-if [ "$LIBTOOLIZE" == "" ] && [ "`uname`" == "Darwin" ]; then
+if [ "$LIBTOOLIZE" = "" ] && [ "`uname`" = "Darwin" ]; then
   LIBTOOLIZE=glibtoolize
 fi
 
