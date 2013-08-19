libuv/libuv

commit 5ff6f85f5872c5096d05d7da52900f68383b8f90

darwin: call pthread_setname_np() if available
When setting the process title, also call pthread_setname_np() when
supported (OS X >= 10.6 and iOS >= 3.2.)

src/unix/darwin-proctitle.c
@@ -19,6 +19,7 @@
  */
 
 #include <TargetConditionals.h>
+#include <dlfcn.h>
 
 #if !TARGET_OS_IPHONE
 # include <CoreFoundation/CoreFoundation.h>
@@ -26,9 +27,30 @@
 #endif
 
 
+static int uv__pthread_setname_np(const char* name) {
+  int (*dynamic_pthread_setname_np)(const char* name);
+  char namebuf[64];  /* MAXTHREADNAMESIZE */
+  int err;
+
+  /* pthread_setname_np() first appeared in OS X 10.6 and iOS 3.2. */
+  dynamic_pthread_setname_np = dlsym(RTLD_DEFAULT, "pthread_setname_np");
+  if (dynamic_pthread_setname_np == NULL)
+    return -ENOSYS;
+
+  strncpy(namebuf, name, sizeof(namebuf) - 1);
+  namebuf[sizeof(namebuf) - 1] = '\0';
+
+  err = dynamic_pthread_setname_np(namebuf);
+  if (err)
+    return -err;
+
+  return 0;
+}
+
+
 int uv__set_process_title(const char* title) {
 #if TARGET_OS_IPHONE
-  return -ENOSYS;
+  return uv__pthread_setname_np(title);
 #else
   typedef CFTypeRef (*LSGetCurrentApplicationASNType)(void);
   typedef OSStatus (*LSSetApplicationInformationItemType)(int,
@@ -83,6 +105,8 @@ int uv__set_process_title(const char* title) {
                                             NULL);
   if (err != noErr)
     return -ENOENT;
+
+  uv__pthread_setname_np(title);  /* Don't care if it fails. */
 
   return 0;
 #endif  /* !TARGET_OS_IPHONE */

commit b03192ed6f1ad6f1978d91a4e1d0d7fdda01a6da

darwin: fix ios build error
Include <errno.h> and <stdlib.h>.  They're being pulled in implicitly
on OS X but apparently that's not the case with iOS builds.

Build breakage introduced in 5ff6f85 by yours truly. Mea culpa.

Fixes #885 and #891.

src/unix/darwin-proctitle.c
@@ -18,8 +18,11 @@
  * IN THE SOFTWARE.
  */
 
-#include <TargetConditionals.h>
 #include <dlfcn.h>
+#include <errno.h>
+#include <stdlib.h>
+
+#include <TargetConditionals.h>
 
 #if !TARGET_OS_IPHONE
 # include <CoreFoundation/CoreFoundation.h>