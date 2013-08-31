bdc828972a24fea1da229ab5f92bc5598cc54415
Ben Noordhuis <info@bnoordhuis.nl>
2013-08-31 04:55:42 +0200

    windows: fix flags assignment in uv_fs_readdir()

--- src/win/fs.c
+++ src/win/fs.c
@@ -1672,7 +1672,7 @@ int uv_fs_readdir(uv_loop_t* loop, uv_fs_t* req, const char* path, int flags,
     return uv_translate_sys_error(err);
   }
 
-  req->file_flags;
+  req->file_flags = flags;
 
   if (cb) {
     QUEUE_FS_TP_JOB(loop, req);
