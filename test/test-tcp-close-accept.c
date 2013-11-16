0c05532e1398ae986d35ba20573ac0a72ba7b4ab
Ben Noordhuis <info@bnoordhuis.nl>
2013-11-16 21:55:55 +0100

    test: don't connect to 0.0.0.0
    OpenBSD won't let you do that, the connect() system call fails with
    an EINVAL error.  Connect to 127.0.0.1 instead.

--- test/test-tcp-close-accept.c
+++ test/test-tcp-close-accept.c
@@ -153,7 +153,7 @@ TEST_IMPL(tcp_close_accept) {
    */
 
   loop = uv_default_loop();
-  ASSERT(0 == uv_ip4_addr("0.0.0.0", TEST_PORT, &addr));
+  ASSERT(0 == uv_ip4_addr("127.0.0.1", TEST_PORT, &addr));
 
   ASSERT(0 == uv_tcp_init(loop, &tcp_server));
   ASSERT(0 == uv_tcp_bind(&tcp_server, (const struct sockaddr*) &addr));
