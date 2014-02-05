84f305915fd45c1e55d261db565b3ddfdbc9d4ce
Geoffry Song <goffrie@gmail.com>
2014-02-05 18:36:24 -0500

    linux: always deregister closing fds from epoll
    If the same file description is open in two different processes, then
    closing the file descriptor is not sufficient to deregister it from the
    epoll instance (as described in epoll(7)), resulting in spurious events
    that cause the event loop to spin repeatedly. So always explicitly
    deregister it.
    
    Fixes #1099.
    
    Conflicts:
    	test/test-spawn.c

--- test/test-list.h
+++ test/test-list.h
@@ -226,6 +226,7 @@ TEST_DECLARE   (spawn_setuid_setgid)
 TEST_DECLARE   (we_get_signal)
 TEST_DECLARE   (we_get_signals)
 TEST_DECLARE   (signal_multiple_loops)
+TEST_DECLARE   (closed_fd_events)
 #endif
 #ifdef __APPLE__
 TEST_DECLARE   (osx_select)
@@ -458,6 +459,7 @@ TASK_LIST_START
   TEST_ENTRY  (we_get_signal)
   TEST_ENTRY  (we_get_signals)
   TEST_ENTRY  (signal_multiple_loops)
+  TEST_ENTRY  (closed_fd_events)
 #endif
 
 #ifdef __APPLE__
