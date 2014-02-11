c8e4db6e00186c7feb100027f518efb070431d80
Saúl Ibarra Corretgé <saghul@gmail.com>
2014-02-11 23:56:50 +0100

    include: mark close_cb field as private

--- include/uv.h
+++ include/uv.h
@@ -499,12 +499,12 @@ struct uv_shutdown_s {
 
 #define UV_HANDLE_FIELDS                                                      \
   /* public */                                                                \
-  uv_close_cb close_cb;                                                       \
   void* data;                                                                 \
   /* read-only */                                                             \
   uv_loop_t* loop;                                                            \
   uv_handle_type type;                                                        \
   /* private */                                                               \
+  uv_close_cb close_cb;                                                       \
   void* handle_queue[2];                                                      \
   UV_HANDLE_PRIVATE_FIELDS                                                    \
 
