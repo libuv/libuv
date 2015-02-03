8651cadd4c831930cc9785d338883d68837e7a36
Brett Vickers <brett@beevik.com>
2015-02-03 10:07:36 -0800

    Coding style improvements. Renamed uv_free/uv_malloc to uv__free/uv__malloc. Renamed uv_free_cb/uv_malloc_cb to uv_free_func/uv_malloc_func.

--- include/uv.h
+++ include/uv.h
@@ -247,6 +247,7 @@ typedef void* (*uv_malloc_func)(size_t size);
 typedef void (*uv_free_func)(void* ptr);
 
 UV_EXTERN void uv_replace_allocator(uv_malloc_func malloc_func, uv_free_func free_func);
+
 UV_EXTERN uv_loop_t* uv_default_loop(void);
 UV_EXTERN int uv_loop_init(uv_loop_t* loop);
 UV_EXTERN int uv_loop_close(uv_loop_t* loop);
