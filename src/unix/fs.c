7c707fe28348a819b0d4d61ca705ee35ae0102c5
Massimiliano Torromeo <massimiliano.torromeo@gmail.com>
2015-02-19 16:58:42 +0100

    unix: fixed glibc-2.20+ macro incompatibility
    Since glibc-2.20 the _SVID_SOURCE macro is deprecated and the newer
    _DEFAULT_SOURCE macro should be used instead.
    
    This fixed bug #173

--- src/unix/fs.c
+++ src/unix/fs.c
@@ -661,8 +661,12 @@ static void uv__to_stat(struct stat* src, uv_stat_t* dst) {
   dst->st_birthtim.tv_nsec = src->st_birthtimespec.tv_nsec;
   dst->st_flags = src->st_flags;
   dst->st_gen = src->st_gen;
-#elif !defined(_AIX) && \
-  (defined(_BSD_SOURCE) || defined(_SVID_SOURCE) || defined(_XOPEN_SOURCE))
+#elif !defined(_AIX) && (           \
+        defined(_BSD_SOURCE)     || \
+        defined(_SVID_SOURCE)    || \
+        defined(_XOPEN_SOURCE)   || \
+        defined(_DEFAULT_SOURCE)    \
+      )
   dst->st_atim.tv_sec = src->st_atim.tv_sec;
   dst->st_atim.tv_nsec = src->st_atim.tv_nsec;
   dst->st_mtim.tv_sec = src->st_mtim.tv_sec;
