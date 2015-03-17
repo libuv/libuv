bcda8d9961c4d5ef14b5d740a8f5ec64f7e474b0
Kiran Pamnany <kpamnany@gmail.com>
2015-03-17 17:58:16 +0530

    thread: support for affinity and detach
    uv_thread_create_on() takes a processor number (not a mask), and sets
    the thread's affinity (pthread_setaffinity_np and SetThreadAffinityMask).
    
    uv_thread_setaffinity() sets the specified thread's affinity.
    
    uv_thread_detach() detaches the thread so it will be cleaned up on
    exit automatically--joining it is no longer necessary or possible
    (pthread_detach and CloseHandle).

--- include/uv.h
+++ include/uv.h
@@ -1392,6 +1392,12 @@ UV_EXTERN void uv_key_set(uv_key_t* key, void* value);
 typedef void (*uv_thread_cb)(void* arg);
 
 UV_EXTERN int uv_thread_create(uv_thread_t* tid, uv_thread_cb entry, void* arg);
+UV_EXTERN int uv_thread_create_on(int proc_num,
+                                  uv_thread_t* tid,
+                                  uv_thread_cb entry,
+                                  void *arg);
+UV_EXTERN int uv_thread_setaffinity(uv_thread_t* tid, int proc_num);
+UV_EXTERN int uv_thread_detach(uv_thread_t* tid);
 UV_EXTERN uv_thread_t uv_thread_self(void);
 UV_EXTERN int uv_thread_join(uv_thread_t *tid);
 UV_EXTERN int uv_thread_equal(const uv_thread_t* t1, const uv_thread_t* t2);
