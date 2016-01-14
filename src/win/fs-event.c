3c59ad6d9520bd3edadc84d15402582caf75dec2
Nikolai Vavilov <vvnicholas@gmail.com>
2016-01-14 19:21:47 +0200

    win: fix watching root files
    When passing "\\?\C:" to CreateFile, it opens the drive rather than the root
    directory. So include the trailing backslash in the directory name.
    
    Fixes: https://github.com/nodejs/node/issues/4643
    PR-URL: https://github.com/libuv/libuv/pull/689
    Reviewed-By: Saúl Ibarra Corretgé <saghul@gmail.com>

--- src/win/fs-event.c
+++ src/win/fs-event.c
@@ -101,12 +101,12 @@ static int uv_split_path(const WCHAR* filename, WCHAR** dir,
     *file = wcsdup(filename);
   } else {
     if (dir) {
-      *dir = (WCHAR*)uv__malloc((i + 1) * sizeof(WCHAR));
+      *dir = (WCHAR*)uv__malloc((i + 2) * sizeof(WCHAR));
       if (!*dir) {
         uv_fatal_error(ERROR_OUTOFMEMORY, "uv__malloc");
       }
-      wcsncpy(*dir, filename, i);
-      (*dir)[i] = L'\0';
+      wcsncpy(*dir, filename, i + 1);
+      (*dir)[i + 1] = L'\0';
     }
 
     *file = (WCHAR*)uv__malloc((len - i) * sizeof(WCHAR));
