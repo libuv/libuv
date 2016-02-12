96da21d3f44c571f30052d9b49ba9bceaf9ffba4
Rasmus Christian Pedersen <zerhacken@yahoo.com>
2016-02-12 14:09:35 +0100

    build: python 2.x/3.x consistent print usage
    PR-URL: https://github.com/libuv/libuv/pull/723
    Reviewed-By: Saúl Ibarra Corretgé <saghul@gmail.com>

--- gyp_uv.py
+++ gyp_uv.py
@@ -37,7 +37,7 @@ def host_arch():
 def run_gyp(args):
   rc = gyp.main(args)
   if rc != 0:
-    print 'Error running GYP'
+    print('Error running GYP')
     sys.exit(rc)
 
 
@@ -90,5 +90,5 @@ if __name__ == '__main__':
     args.append('--no-parallel')
 
   gyp_args = list(args)
-  print gyp_args
+  print(gyp_args)
   run_gyp(gyp_args)
