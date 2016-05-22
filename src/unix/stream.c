bf6d4feeaba53ec135f7d985c4025beffa8cfcd5
Michael Neumann <mneumann@ntecs.de>
2016-05-23 00:02:54 +0200

    unix: Correctly detect named pipes on DragonFly
    This fixes test-stdio-over-pipes and test-pipe-sendmsg test cases.
    
    Before this commit, the following assertions were raised ("make check"):
    
        Assertion failed in test/test-stdio-over-pipes.c on line 56: term_signal == 0
        Assertion failed in test/test-pipe-sendmsg.c on line 86: pending == UV_NAMED_PIPE
    
    This fixes a longstanding issue with nodejs on DragonFly, which was
    triggered whenever spawning a process and pipes were involed.

--- src/unix/stream.c
+++ src/unix/stream.c
@@ -962,8 +962,8 @@ uv_handle_type uv__handle_type(int fd) {
     return UV_UNKNOWN_HANDLE;
 
   if (type == SOCK_STREAM) {
-#if defined(_AIX)
-    /* on AIX the getsockname call returns an empty sa structure
+#if defined(_AIX) || defined(__DragonFly__)
+    /* on AIX/DragonFly the getsockname call returns an empty sa structure
      * for sockets of type AF_UNIX.  For all other types it will
      * return a properly filled in structure.
      */
