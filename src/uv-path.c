6a7e4af975f67b8db24b85735b87f51e2d87aea7
Recep Aslantas <m@recp.me>
2016-07-11 02:09:06 +0300

    improve

--- src/uv-path.c
+++ src/uv-path.c
@@ -29,14 +29,22 @@ int
 uv_path_join(char **fragments,
              char *buf,
              size_t *size) {
-#define SLASH       '/'
-#define BACK_SLASH  '\\'
-#define SLASH_STR   "/"
+#define CHR_SLASH       '/'
+#define CHR_BACK_SLASH  '\\'
+#define CHR_COLON       ':'
+#define STR_SLASH       "/"
+#define APPEND_SLASH \
+  do { \
+    strncpy(buf++, STR_SLASH, 1); \
+    len++; \
+  } while (0)
+
   const char *frag;
   const char *frag_end;
   size_t      len;
   size_t      frag_len;
   int         frag_idx;
+  int         frag_idx_v;
   char        c;
 
   if (!fragments
@@ -45,63 +53,74 @@ uv_path_join(char **fragments,
       || !size)
     return -EINVAL;
 
-  len = 0;
-
-  /* 
-   dont trim sep from beginning of first fragment (etc: /usr)
-   */
-  frag     = *fragments;
-  frag_end = frag + strlen(frag) - 1;
-  c        = *frag_end;
-
-  while (c != '\0'
-         && (c == SLASH || c == BACK_SLASH))
-    c = *--frag_end;
-
-  frag_end++;
-
-  len += frag_len = frag_end - frag;
-  if (*size < len)
-    return -1;
-  
-  strncpy(buf, frag, frag_len);
-
-  buf += frag_len;
+  frag_idx_v = frag_idx = len = 0;
 
   /* build path */
-  frag_idx = 1;
-
   while ((frag = fragments[frag_idx++])) {
-    frag_end = frag + strlen(frag) - 1;
+    frag_len = strlen(frag);
+    frag_end = frag + frag_len - 1;
+
+    if (frag_len == 0)
+      continue;
+
+    /* starts with slash e.g. /usr */
+    c = *frag;
+    if (frag_idx_v == 0
+        && (c == CHR_SLASH || c == CHR_BACK_SLASH)) {
+      APPEND_SLASH;
+      frag++;
+
+      /* avoid extra slash after non-protocol frag 
+         like /http:/ -> /http:// 
+       */
+      frag_idx_v++;
+    }
 
     /* l-trim */
     while ((c = *frag) != '\0'
-           && (c == SLASH || c == BACK_SLASH))
+           && (c == CHR_SLASH || c == CHR_BACK_SLASH))
       frag++;
 
     /* r-trim */
     while ((c = *frag_end) != '\0'
-           && (c == SLASH || c == BACK_SLASH))
+           && (c == CHR_SLASH || c == CHR_BACK_SLASH))
       frag_end--;
 
-    frag_end++;
+    /* only slashes */
+    if (!*frag) {
+      if (len == 0)
+        APPEND_SLASH;
 
-    len += frag_len = frag_end - frag;
+      continue;
+    }
+
+    if (len > 1 && *(buf - 1) != CHR_SLASH)
+      APPEND_SLASH;
+
+    /* protocol e.g. http://, tcp:// */
+    if (frag_idx_v == 1 && *(buf - 2) == CHR_COLON)
+      APPEND_SLASH;
+
+    frag_len = ++frag_end - frag;
+    len += frag_len;
+
     if (*size < len)
       return -1;
 
-    strncpy(buf++, SLASH_STR, 1);
     strncpy(buf, frag, frag_len);
 
     buf += frag_len;
-  }
+    frag_idx_v++;
+  };
 
   memset(buf + 1, '\0', 1);
   *size = len;
 
   return 0;
 
-#undef SLASH_STR
-#undef BACK_SLASH
-#undef SLASH
+#undef APPEND_SLASH
+#undef STR_SLASH
+#undef CHR_COLON
+#undef CHR_BACK_SLASH
+#undef CHR_SLASH
 }
