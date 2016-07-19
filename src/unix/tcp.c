f4ef8976666db685a22379e76b397ac6aefed900
John Barboza <jbarboza@ca.ibm.com>
2016-07-19 09:07:28 -0400

    zos: track unbound handles and bind before listen
    On zOS the listen call does not bind automatically if the socket
    is unbound. Hence the manual binding to an arbitrary port is
    required to be done manually.
    
    PR-URL: https://github.com/libuv/libuv/pull/949
    Reviewed-By: Saúl Ibarra Corretgé <saghul@gmail.com>

--- src/unix/tcp.c
+++ src/unix/tcp.c
@@ -134,6 +134,7 @@ int uv__tcp_bind(uv_tcp_t* tcp,
   }
   tcp->delayed_error = -errno;
 
+  tcp->flags |= UV_HANDLE_BOUND;
   if (addr->sa_family == AF_INET6)
     tcp->flags |= UV_HANDLE_IPV6;
 
@@ -276,10 +277,32 @@ int uv_tcp_listen(uv_tcp_t* tcp, int backlog, uv_connection_cb cb) {
   if (err)
     return err;
 
+#ifdef __MVS__
+  /* on zOS the listen call does not bind automatically
+     if the socket is unbound. Hence the manual binding to
+     an arbitrary port is required to be done manually
+  */
+
+  if (!(tcp->flags & UV_HANDLE_BOUND)) {
+    struct sockaddr_storage saddr;
+    socklen_t slen  = sizeof(saddr);
+    memset(&saddr, 0, sizeof(saddr));
+
+    if (getsockname(tcp->io_watcher.fd, (struct sockaddr*) &saddr, &slen))
+      return -errno;
+
+    if (bind(tcp->io_watcher.fd, (struct sockaddr*) &saddr, slen))
+      return -errno;
+
+    tcp->flags |= UV_HANDLE_BOUND;
+  }
+#endif
+
   if (listen(tcp->io_watcher.fd, backlog))
     return -errno;
 
   tcp->connection_cb = cb;
+  tcp->flags |= UV_HANDLE_BOUND;
 
   /* Start listening for connections. */
   tcp->io_watcher.cb = uv__server_io;
