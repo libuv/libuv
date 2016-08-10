250eaf43994bbe67218423a83c19d402da5b9338
Saúl Ibarra Corretgé <saghul@gmail.com>
2016-08-10 22:39:26 +0100

    Revert "win,tty: remove no longer needed workaround"
    This reverts commit 7892bd6f767b6969f2d6f767523500227f15a6f0.
    
    The reasoning for the original patch was flawed. On x86 systems
    _InterlockedOr intrinsic must be used. MinGW also exposes the problem,
    so reverting this gets rid of a warning.
    
    PR-URL: https://github.com/libuv/libuv/pull/990
    Reviewed-By: Ben Noordhuis <info@bnoordhuis.nl>
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>

--- src/win/tty.c
+++ src/win/tty.c
@@ -35,6 +35,10 @@
 #include "stream-inl.h"
 #include "req-inl.h"
 
+#ifndef InterlockedOr
+# define InterlockedOr _InterlockedOr
+#endif
+
 #define UNICODE_REPLACEMENT_CHARACTER (0xfffd)
 
 #define ANSI_NORMAL           0x00
