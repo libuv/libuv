d00de9321b698e2074919b638d0a2ee087077a64
Ben Noordhuis <info@bnoordhuis.nl>
2016-10-14 12:43:02 +0200

    win: remove unused static variables
    default_loop_struct and default_loop_ptr seem to have been overlooked
    in commit 32747c7 ("win,unix: move loop functions which have identical
    implementations".)
    
    PR-URL: https://github.com/libuv/libuv/pull/1097
    Reviewed-By: Andrius Bentkus <andrius.bentkus@gmail.com>
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>
    Reviewed-By: Santiago Gimeno <santiago.gimeno@gmail.com>
    Reviewed-By: Saúl Ibarra Corretgé <saghul@gmail.com>

--- src/win/core.c
+++ src/win/core.c
@@ -35,10 +35,6 @@
 #include "handle-inl.h"
 #include "req-inl.h"
 
-
-static uv_loop_t default_loop_struct;
-static uv_loop_t* default_loop_ptr;
-
 /* uv_once initialization guards */
 static uv_once_t uv_init_guard_ = UV_ONCE_INIT;
 
