a6bf4c69224c4ba8e615d551fdc22908c3aff1e9
Saúl Ibarra Corretgé <saghul@gmail.com>
2016-10-21 10:08:06 -0300

    Revert "unix,win: simplify calculating polling timeout"
    This reverts commit a35308306fc9614407194fd294fdc130c98b53a5.

--- src/unix/core.c
+++ src/unix/core.c
@@ -301,13 +301,16 @@ int uv_backend_timeout(const uv_loop_t* loop) {
   if (loop->stop_flag != 0)
     return 0;
 
-  if (!uv_loop_alive(loop))
+  if (!uv__has_active_handles(loop) && !uv__has_active_reqs(loop))
     return 0;
 
   if (!QUEUE_EMPTY(&loop->idle_handles))
     return 0;
 
   if (!QUEUE_EMPTY(&loop->pending_queue))
+    return 0;
+
+  if (loop->closing_handles)
     return 0;
 
   return uv__next_timeout(loop);
