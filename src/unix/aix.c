ed2dcf67b0e09611213eaaaeb16d175b7bbbaa7c
Howard Hellyer <hhellyer@uk.ibm.com>
2017-01-24 13:57:46 +0000

    aix: enable uv_get/set_process_title
    Don't leave a bad pointer in process_argv[0] on malloc failure.
    Fix typo in help for uv_set_process_title

--- src/unix/aix.c
+++ src/unix/aix.c
@@ -929,18 +929,22 @@ char** uv_setup_args(int argc, char** argv) {
 
 
 int uv_set_process_title(const char* title) {
+  char* new_title;
+
+  /* We cannot free this pointer when libuv shuts down,
+   * the process may still be using it.
+   */
+  new_title = uv__strdup(title);
+  if (new_title == NULL)
+    return -ENOMEM;
+
   /* If this is the first time this is set,
    * don't free and set argv[1] to NULL.
    */
   if (process_title_ptr != NULL)
     uv__free(process_title_ptr);
 
-  /* We cannot free this pointer when libuv shuts down,
-   * the process may still be using it.
-   */
-  process_title_ptr = uv__strdup(title);
-  if (process_title_ptr == NULL)
-    return -ENOMEM;
+  process_title_ptr = new_title;
 
   process_argv[0] = process_title_ptr;
   if (process_argc > 1)
