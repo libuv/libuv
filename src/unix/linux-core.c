76bcf9f91d8dc13b2051b20e8e21cb7f62b52874
Brad King <brad.king@kitware.com>
2017-04-04 15:39:36 -0400

    unix: factor out reusable sysinfo loadavg impl
    On Linux we use `sysinfo(2)` to look up the load average.
    Factor out a dedicated source file to make this implementation
    available for use on other platforms that support it.
    
    PR-URL: https://github.com/libuv/libuv/pull/1312
    Reviewed-By: Ben Noordhuis <info@bnoordhuis.nl>
    Reviewed-By: Santiago Gimeno <santiago.gimeno@gmail.com>

--- src/unix/linux-core.c
+++ src/unix/linux-core.c
@@ -472,17 +472,6 @@ uint64_t uv__hrtime(uv_clocktype_t type) {
 }
 
 
-void uv_loadavg(double avg[3]) {
-  struct sysinfo info;
-
-  if (sysinfo(&info) < 0) return;
-
-  avg[0] = (double) info.loads[0] / 65536.0;
-  avg[1] = (double) info.loads[1] / 65536.0;
-  avg[2] = (double) info.loads[2] / 65536.0;
-}
-
-
 int uv_exepath(char* buffer, size_t* size) {
   ssize_t n;
 
