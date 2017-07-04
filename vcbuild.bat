65d407b5b1319bd7690c9bddf945cd58d121be9c
Refael Ackermann <refack@gmail.com>
2017-07-04 08:46:22 -0400

    "documented" the vs2017 option

--- vcbuild.bat
+++ vcbuild.bat
@@ -168,7 +168,7 @@ echo Failed to create vc project files.
 exit /b 1
 
 :help
-echo vcbuild.bat [debug/release] [test/bench] [clean] [noprojgen] [nobuild] [x86/x64] [static/shared]
+echo vcbuild.bat [debug/release] [test/bench] [clean] [noprojgen] [nobuild] [vs2017] [x86/x64] [static/shared]
 echo Examples:
 echo   vcbuild.bat              : builds debug build
 echo   vcbuild.bat test         : builds debug build and runs tests
