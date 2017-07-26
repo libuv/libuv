27dcbd6ff41f64dd8ecae67b0587523c94c14388
Saúl Ibarra Corretgé <saghul@gmail.com>
2017-07-26 22:01:03 +0200

    android: remove no longer needed check
    The latest NDK (r15 at the time of this writing) no longer needs this.
    `pthread_cond_timedwait` is available at any (reasonable) API level.
    
    PR-URL: https://github.com/libuv/libuv/pull/1441
    Reviewed-By: Ben Noordhuis <info@bnoordhuis.nl>
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>

--- src/unix/thread.c
+++ src/unix/thread.c
@@ -615,15 +615,7 @@ int uv_cond_timedwait(uv_cond_t* cond, uv_mutex_t* mutex, uint64_t timeout) {
   timeout += uv__hrtime(UV_CLOCK_PRECISE);
   ts.tv_sec = timeout / NANOSEC;
   ts.tv_nsec = timeout % NANOSEC;
-#if defined(__ANDROID__) && defined(HAVE_PTHREAD_COND_TIMEDWAIT_MONOTONIC)
-  /*
-   * The bionic pthread implementation doesn't support CLOCK_MONOTONIC,
-   * but has this alternative function instead.
-   */
-  r = pthread_cond_timedwait_monotonic_np(cond, mutex, &ts);
-#else
   r = pthread_cond_timedwait(cond, mutex, &ts);
-#endif /* __ANDROID__ */
 #endif
 
 
