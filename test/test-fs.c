misterdjules/libuv-1

unix/fs.c: Fix for potential partial reads/writes
Added a check in uv__fs_buf_iter to detect partial reads and writes.
Partial reads and writes are looped until all data has been processed.

test/test-fs.c
@@ -2684,3 +2684,125 @@ TEST_IMPL(fs_read_write_null_arguments) {
 
   return 0;
 }
+
+#ifdef _WIN32
+
+TEST_IMPL(fs_partial_read) {
+  RETURN_SKIP("Test not implemented on Windows.");
+}
+
+TEST_IMPL(fs_partial_write) {
+  RETURN_SKIP("Test not implemented on Windows.");
+}
+
+#else  /* !_WIN32 */
+
+static void thread_exec(int fd, char* data, int size, int interval, int doread) {
+  const pid_t pid = getpid();
+  ssize_t result = 1;
+  int error = 0;
+
+  while (size > 0 && error == 0) {
+    do {
+      if(doread)
+        result = write(fd, data, size < interval ? size : interval);
+      else
+        result = read(fd, data, size < interval ? size : interval);
+
+      error = errno;
+    } while (result == -1 && error == EINTR);
+
+    kill(pid, SIGUSR1);
+    if (result > 0) {
+      error = 0;
+      size -= result;
+      data += result;
+    }
+  }
+  ASSERT(size == 0);
+  ASSERT(result > 0);
+}
+
+struct thread_ctx {
+  int fd;
+  char *data;
+  int size;
+  int interval;
+  int doread;
+};
+
+static void thread_main(void* arg) {
+  struct thread_ctx *ctx = (struct thread_ctx*)arg;
+  thread_exec(ctx->fd, ctx->data, ctx->size, ctx->interval, ctx->doread);
+}
+
+static void sig_func(uv_signal_t* handle, int signum) {
+  uv_signal_stop(handle);
+}
+
+static void test_fs_partial(int doread) {
+  struct thread_ctx ctx;
+  uv_thread_t thread;
+  uv_signal_t signal;
+  int pipe_fds[2];
+  const size_t iovcount = 54321;
+  uv_buf_t* iovs;
+  char* buffer;
+  size_t index;
+  int result;
+
+  iovs = malloc(sizeof(*iovs) * iovcount);
+  ASSERT(iovs != NULL);
+
+  ctx.doread = doread;
+  ctx.interval = 1000;
+  ctx.size = sizeof(test_buf) * iovcount;
+  ctx.data = malloc(ctx.size);
+  buffer = malloc(ctx.size);
+
+  for (index = 0; index < iovcount; ++index)
+	iovs[index] = uv_buf_init(buffer + index * sizeof(test_buf), sizeof(test_buf));
+
+  loop = uv_default_loop();
+
+  ASSERT(0 == uv_signal_init(loop, &signal));
+  ASSERT(0 == uv_signal_start(&signal, sig_func, SIGUSR1));
+
+  ASSERT(0 == pipe(pipe_fds));
+
+  ctx.fd = pipe_fds[doread];
+  ASSERT(0 == uv_thread_create(&thread, thread_main, &ctx));
+
+  if(doread)
+    result = uv_fs_read(loop, &read_req, pipe_fds[0], iovs, iovcount, -1, NULL);
+  else
+    result = uv_fs_write(loop, &write_req, pipe_fds[1], iovs, iovcount, -1, NULL);
+
+  ASSERT(result == ctx.size);
+  ASSERT(0 == memcmp(buffer, ctx.data, result));
+
+  ASSERT(0 == uv_thread_join(&thread));
+  ASSERT(0 == uv_run(loop, UV_RUN_DEFAULT));
+
+  ASSERT(0 == close(pipe_fds[0]));
+  ASSERT(0 == close(pipe_fds[1]));
+  uv_close((uv_handle_t*) &signal, NULL);
+
+  free(iovs);
+  free(buffer);
+  free(ctx.data);
+
+  MAKE_VALGRIND_HAPPY();
+}
+
+TEST_IMPL(fs_partial_read) {
+  test_fs_partial(1);
+  return 0;
+}
+
+TEST_IMPL(fs_partial_write) {
+  test_fs_partial(0);
+  return 0;
+}
+
+#endif/* _WIN32 */

libuv/libuv

test/test-fs.c
@@ -2748,6 +2748,122 @@ TEST_IMPL(fs_write_alotof_bufs_with_offset) {
 }
 
 
+#ifdef _WIN32
+
+TEST_IMPL(fs_partial_read) {
+  RETURN_SKIP("Test not implemented on Windows.");
+}
+
+TEST_IMPL(fs_partial_write) {
+  RETURN_SKIP("Test not implemented on Windows.");
+}
+
+#else  /* !_WIN32 */
+
+static void thread_exec(int fd, char* data, int size, int interval, int doread) {
+  const pid_t pid = getpid();
+  ssize_t result = 1;
+
+  while (size > 0 && result > 0) {
+    do {
+      if(doread)
+        result = write(fd, data, size < interval ? size : interval);
+      else
+        result = read(fd, data, size < interval ? size : interval);
+    } while (result == -1 && errno == EINTR);
+
+    kill(pid, SIGUSR1);
+    size -= result;
+    data += result;
+  }
+  ASSERT(size == 0);
+  ASSERT(result > 0);
+}
+
+struct thread_ctx {
+  int fd;
+  char *data;
+  int size;
+  int interval;
+  int doread;
+};
+
+static void thread_main(void* arg) {
+  struct thread_ctx *ctx = (struct thread_ctx*)arg;
+  thread_exec(ctx->fd, ctx->data, ctx->size, ctx->interval, ctx->doread);
+}
+
+static void sig_func(uv_signal_t* handle, int signum) {
+  uv_signal_stop(handle);
+}
+
+static void test_fs_partial(int doread) {
+  struct thread_ctx ctx;
+  uv_thread_t thread;
+  uv_signal_t signal;
+  int pipe_fds[2];
+  const size_t iovcount = 54321;
+  uv_buf_t* iovs;
+  char* buffer;
+  size_t index;
+  int result;
+
+  iovs = malloc(sizeof(*iovs) * iovcount);
+  ASSERT(iovs != NULL);
+
+  ctx.doread = doread;
+  ctx.interval = 1000;
+  ctx.size = sizeof(test_buf) * iovcount;
+  ctx.data = malloc(ctx.size);
+  buffer = malloc(ctx.size);
+
+  for (index = 0; index < iovcount; ++index)
+	iovs[index] = uv_buf_init(buffer + index * sizeof(test_buf), sizeof(test_buf));
+
+  loop = uv_default_loop();
+
+  ASSERT(0 == uv_signal_init(loop, &signal));
+  ASSERT(0 == uv_signal_start(&signal, sig_func, SIGUSR1));
+
+  ASSERT(0 == pipe(pipe_fds));
+
+  ctx.fd = pipe_fds[doread];
+  ASSERT(0 == uv_thread_create(&thread, thread_main, &ctx));
+
+  if(doread)
+    result = uv_fs_read(loop, &read_req, pipe_fds[0], iovs, iovcount, -1, NULL);
+  else
+    result = uv_fs_write(loop, &write_req, pipe_fds[1], iovs, iovcount, -1, NULL);
+
+  ASSERT(result == ctx.size);
+  ASSERT(0 == memcmp(buffer, ctx.data, result));
+
+  ASSERT(0 == uv_thread_join(&thread));
+  ASSERT(0 == uv_run(loop, UV_RUN_DEFAULT));
+
+  ASSERT(0 == close(pipe_fds[0]));
+  ASSERT(0 == close(pipe_fds[1]));
+  uv_close((uv_handle_t*) &signal, NULL);
+
+  free(iovs);
+  free(buffer);
+  free(ctx.data);
+
+  MAKE_VALGRIND_HAPPY();
+}
+
+TEST_IMPL(fs_partial_read) {
+  test_fs_partial(1);
+  return 0;
+}
+
+TEST_IMPL(fs_partial_write) {
+  test_fs_partial(0);
+  return 0;
+}
+
+#endif/* _WIN32 */
+
 TEST_IMPL(fs_read_write_null_arguments) {
   int r;
 