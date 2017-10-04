957b112f99035f48eb733d55b44d62888505c192
Bartosz Sosnowski <bartosz@janeasystems.com>
2017-10-04 11:08:37 +0200

    fixup: remove unused things

--- src/win/dl.c
+++ src/win/dl.c
@@ -89,7 +89,6 @@ static void uv__format_fallback_error(uv_lib_t* lib, int errorno){
 
 
 static int uv__dlerror(uv_lib_t* lib, const char* filename, DWORD errorno) {
-  static const char not_win32_app_msg[] = "%1 is not a valid Win32 application";
   DWORD_PTR arg;
   DWORD res;
   char* msg;
@@ -115,7 +114,6 @@ static int uv__dlerror(uv_lib_t* lib, const char* filename, DWORD errorno) {
                          0, (LPSTR) &lib->errmsg, 0, NULL);
   }
 
-  /* Inexpert hack to get the filename into the error message. */
   if (res && errorno == ERROR_BAD_EXE_FORMAT && strstr(lib->errmsg, "%1")) {
     msg = lib->errmsg;
     lib->errmsg = NULL;
