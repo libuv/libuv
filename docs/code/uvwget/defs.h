162b7f47f17f0e0a07f35a904bfa5ceef7d27b21
rayrase <rmartinez2175@eagle.fgcu.edu>
2017-10-05 17:47:04 -0400

    added header guard and replaced container_of to use offset_of for clarity

--- docs/code/uvwget/defs.h
+++ docs/code/uvwget/defs.h
@@ -1,2 +1,10 @@
+#ifndef DEFS_H_
+#define DEFS_H_
+
+#include <stddef.h> 
+
 #define CONTAINER_OF(ptr, type, field)                                        \
-  ((type *) ((char *) (ptr) - ((char *) &((type *) 0)->field)))
+  ((type *) ((char *) (ptr) - offsetof(type, field)))
+
+#endif /* DEFS_H */
+
