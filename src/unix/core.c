e57e07172eba9df2f8fa0cec051cac0c5cac9124
cjihrig <cjihrig@gmail.com>
2018-08-12 21:59:22 -0400

    unix,win: add uv_os_{get,set}priority()
    Refs: https://github.com/nodejs/node/pull/21675
    PR-URL: https://github.com/libuv/libuv/pull/1945
    Reviewed-By: Santiago Gimeno <santiago.gimeno@gmail.com>
    Reviewed-By: Richard Lau <riclau@uk.ibm.com>

--- src/unix/core.c
+++ src/unix/core.c
@@ -1350,3 +1350,31 @@ uv_pid_t uv_os_getpid(void) {
 uv_pid_t uv_os_getppid(void) {
   return getppid();
 }
+
+
+int uv_os_getpriority(uv_pid_t pid, int* priority) {
+  int r;
+
+  if (priority == NULL)
+    return UV_EINVAL;
+
+  errno = 0;
+  r = getpriority(PRIO_PROCESS, (int) pid);
+
+  if (r == -1 && errno != 0)
+    return UV__ERR(errno);
+
+  *priority = r;
+  return 0;
+}
+
+
+int uv_os_setpriority(uv_pid_t pid, int priority) {
+  if (priority < UV_PRIORITY_HIGHEST || priority > UV_PRIORITY_LOW)
+    return UV_EINVAL;
+
+  if (setpriority(PRIO_PROCESS, (int) pid, priority) != 0)
+    return UV__ERR(errno);
+
+  return 0;
+}
