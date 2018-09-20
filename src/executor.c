4c2111b9e942148b5f3d2b99a3f5db9f59e3ebf0
Jamie Davis <davisjam@vt.edu>
2018-09-20 15:23:15 -0400

    PTP: copyright in new files

--- src/executor.c
+++ src/executor.c
@@ -1,4 +1,4 @@
-/* Copyright Joyent, Inc. and other Node contributors. All rights reserved.
+/* Copyright libuv project contributors. All rights reserved.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to
