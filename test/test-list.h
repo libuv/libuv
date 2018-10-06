75fec8ac1007eb4ff1b818d15b327e41cf113957
Sakthipriyan Vairamani (thefourtheye) <thechargingvolcano@gmail.com>
2018-10-07 04:09:22 +0530

    test: make sure that reading a directory fails

--- test/test-list.h
+++ test/test-list.h
@@ -344,6 +344,7 @@ TEST_DECLARE   (fs_partial_read)
 TEST_DECLARE   (fs_partial_write)
 TEST_DECLARE   (fs_file_pos_after_op_with_offset)
 TEST_DECLARE   (fs_null_req)
+TEST_DECLARE   (fs_read_dir_failure)
 #ifdef _WIN32
 TEST_DECLARE   (fs_exclusive_sharing_mode)
 TEST_DECLARE   (fs_open_readonly_acl)
@@ -893,6 +894,7 @@ TASK_LIST_START
   TEST_ENTRY  (fs_read_write_null_arguments)
   TEST_ENTRY  (fs_file_pos_after_op_with_offset)
   TEST_ENTRY  (fs_null_req)
+  TEST_ENTRY  (fs_read_dir_failure)
 #ifdef _WIN32
   TEST_ENTRY  (fs_exclusive_sharing_mode)
   TEST_ENTRY  (fs_open_readonly_acl)
