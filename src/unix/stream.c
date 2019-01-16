122f0f6766d52a4201f408175692b4116f74f8f7
Santiago Gimeno <santiago.gimeno@gmail.com>
2019-01-16 16:14:30 +0100

    unix,stream: fix zero byte writes
    Fixes a regression where a write request to write a zero byte buffer
    would never complete.
    
    Refs: https://github.com/libuv/libuv/pull/2097
    Refs: https://github.com/libuv/libuv/issues/2134
    PR-URL: https://github.com/libuv/libuv/pull/2149
    Reviewed-By: Ben Noordhuis <info@bnoordhuis.nl>

--- src/unix/stream.c
+++ src/unix/stream.c
@@ -745,13 +745,13 @@ static int uv__write_req_update(uv_stream_t* stream,
 
   buf = req->bufs + req->write_index;
 
-  while (n > 0) {
+  do {
     len = n < buf->len ? n : buf->len;
     buf->base += len;
     buf->len -= len;
     buf += (buf->len == 0);  /* Advance to next buffer if this one is empty. */
     n -= len;
-  }
+  } while (n > 0);
 
   req->write_index = buf - req->bufs;
 
@@ -897,7 +897,7 @@ start:
     goto error;
   }
 
-  if (n > 0 && uv__write_req_update(stream, req, n)) {
+  if (n >= 0 && uv__write_req_update(stream, req, n)) {
     uv__write_req_finish(req);
     return;  /* TODO(bnoordhuis) Start trying to write the next request. */
   }
