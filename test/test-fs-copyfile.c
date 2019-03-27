076a9282f2d603aeb92d2ce51a35e9bdbd38aadd
Rich Trott <rtrott@gmail.com>
2019-03-27 11:18:38 -0700

    fixup! fs: add test for copyfile() respecting permissions

--- test/test-fs-copyfile.c
+++ test/test-fs-copyfile.c
@@ -185,6 +185,7 @@ TEST_IMPL(fs_copyfile) {
   if (r == 0)
     handle_result(&req);
 
+#ifndef _WIN32
   /* Copying respects permissions/mode. */
   unlink(dst);
   touch_file(dst, 0);
@@ -193,6 +194,7 @@ TEST_IMPL(fs_copyfile) {
   ASSERT(req.result == UV_EACCES);
   ASSERT(r == UV_EACCES);
   uv_fs_req_cleanup(&req);
+#endif
 
   unlink(dst); /* Cleanup */
   return 0;
