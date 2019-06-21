5fd480678c230b9bf57298d4a9d761c5d2dbd0c7
Ben Noordhuis <info@bnoordhuis.nl>
2019-06-21 11:32:01 +0200

    test: give hrtime test a custom 10s timeout
    The test is supposed to complete in about 3.5s but it can
    hit the 5s timeout when run on a system with high load.
    
    Fixes: https://github.com/libuv/libuv/issues/2342
    PR-URL: https://github.com/libuv/libuv/pull/2345
    Reviewed-By: Colin Ihrig <cjihrig@gmail.com>

--- test/test-list.h
+++ test/test-list.h
@@ -779,7 +779,7 @@ TASK_LIST_START
 
   TEST_ENTRY  (tmpdir)
 
-  TEST_ENTRY  (hrtime)
+  TEST_ENTRY_CUSTOM (hrtime, 0, 0, 10000)
 
   TEST_ENTRY_CUSTOM (getaddrinfo_fail, 0, 0, 10000)
   TEST_ENTRY_CUSTOM (getaddrinfo_fail_sync, 0, 0, 10000)
