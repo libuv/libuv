d05daa19c6ee68ad2b60c53b74feac2d39ddc82d
Thomas Klausner <tk@giga.or.at>
2019-08-22 12:24:31 +0200

    netbsd: check for KERN_PROC_PATHNAME.
    Use fallback implementation in case it doesn't exist.

--- src/unix/netbsd.c
+++ src/unix/netbsd.c
@@ -64,6 +64,7 @@ void uv_loadavg(double avg[3]) {
 
 
 int uv_exepath(char* buffer, size_t* size) {
+#if defined(KERN_PROC_PATHNAME)
   /* Intermediate buffer, retrieving partial path name does not work
    * As of NetBSD-8(beta), vnode->path translator does not handle files
    * with longer names than 31 characters.
@@ -92,6 +93,22 @@ int uv_exepath(char* buffer, size_t* size) {
 
   /* Set new size. */
   *size = strlen(buffer);
+#else
+  ssize_t n;
+
+  if (buffer == NULL || size == NULL || *size == 0)
+    return -EINVAL;
+
+  n = *size - 1;
+  if (n > 0)
+    n = readlink("/proc/self/exe", buffer, n);
+
+  if (n == -1)
+    return -errno;
+
+  buffer[n] = '\0';
+  *size = n;
+#endif
 
   return 0;
 }
