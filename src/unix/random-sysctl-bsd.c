d496b6911ad134484417ebfb59b91ff8643ce889
nia <nia@NetBSD.org>
2019-10-21 20:33:48 +0100

    unix: Add random-sysctl-bsd.
    This uses the kern.arandom sysctl to get entropy from the kernel
    on NetBSD without blocking or requiring /dev/urandom to be opened.

--- src/unix/random-sysctl-bsd.c
+++ src/unix/random-sysctl-bsd.c
@@ -25,71 +25,25 @@
 #include <errno.h>
 #include <string.h>
 
-#include <syscall.h>
+#include <sys/sysctl.h>
 #include <unistd.h>
 
-
-struct uv__sysctl_args {
-  int* name;
-  int nlen;
-  void* oldval;
-  size_t* oldlenp;
-  void* newval;
-  size_t newlen;
-  unsigned long unused[4];
-};
-
-
 int uv__random_sysctl(void* buf, size_t buflen) {
-  static int name[] = {1 /*CTL_KERN*/, 40 /*KERN_RANDOM*/, 6 /*RANDOM_UUID*/};
-  struct uv__sysctl_args args;
-  char uuid[16];
+  static int name[] = {CTL_KERN, KERN_ARND};
+  unsigned char uuid[16];
   char* p;
   char* pe;
-  size_t n;
+  size_t n = sizeof(uuid);
 
   p = buf;
   pe = p + buflen;
 
   while (p < pe) {
-    memset(&args, 0, sizeof(args));
-
-    args.name = name;
-    args.nlen = ARRAY_SIZE(name);
-    args.oldval = uuid;
-    args.oldlenp = &n;
-    n = sizeof(uuid);
-
-    /* Emits a deprecation warning with some kernels but that seems like
-     * an okay trade-off for the fallback of the fallback: this function is
-     * only called when neither getrandom(2) nor /dev/urandom are available.
-     * Fails with ENOSYS on kernels configured without CONFIG_SYSCTL_SYSCALL.
-     * At least arm64 never had a _sysctl system call and therefore doesn't
-     * have a SYS__sysctl define either.
-     */
-#ifdef SYS__sysctl
-    if (syscall(SYS__sysctl, &args) == -1)
+    if (sysctl(name, ARRAY_SIZE(name), &uuid, &n, NULL, 0) == -1)
       return UV__ERR(errno);
-#else
-    {
-      (void) &args;
-      return UV_ENOSYS;
-    }
-#endif
 
     if (n != sizeof(uuid))
       return UV_EIO;  /* Can't happen. */
-
-    /* uuid[] is now a type 4 UUID. Bytes 6 and 8 (counting from zero) contain
-     * 4 and 5 bits of entropy, respectively. For ease of use, we skip those
-     * and only use 14 of the 16 bytes.
-     */
-    uuid[6] = uuid[14];
-    uuid[8] = uuid[15];
-
-    n = pe - p;
-    if (n > 14)
-      n = 14;
 
     memcpy(p, uuid, n);
     p += n;
