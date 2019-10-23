2338f3bfe4c794e01bd9463e0071f820b96490f5
Milad Farazmand <miladfar@ca.ibm.com>
2019-10-23 14:47:15 -0400

    Removing extra line

--- src/unix/fs.c
+++ src/unix/fs.c
@@ -1254,7 +1254,6 @@ static int uv__fs_statx(int fd,
     return UV_ENOSYS; 
   }
   
-
   buf->st_dev = 256 * statxbuf.stx_dev_major + statxbuf.stx_dev_minor;
   buf->st_mode = statxbuf.stx_mode;
   buf->st_nlink = statxbuf.stx_nlink;
