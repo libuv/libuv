57cb0fbfc8179a64d73e5a6616a2b9f6923b753a
Witold Kręcicki <wpk@culm.net>
2020-01-31 09:01:17 +0100

    Revert "unix: remove unused uv__dup() function"
    This reverts commit fab6e64b39f162138fdf9a81765c05490f445f06.

--- src/unix/internal.h
+++ src/unix/internal.h
@@ -194,6 +194,7 @@ int uv__close(int fd); /* preserves errno */
 int uv__close_nocheckstdio(int fd);
 int uv__close_nocancel(int fd);
 int uv__socket(int domain, int type, int protocol);
+int uv__dup(int fd);
 ssize_t uv__recvmsg(int fd, struct msghdr *msg, int flags);
 void uv__make_close_pending(uv_handle_t* handle);
 int uv__getiovmax(void);
