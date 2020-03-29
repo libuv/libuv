188495212815e52574b6d0aa48a42e09f7400914
twosee <twose@qq.com>
2020-03-29 22:56:29 +0800

    Fix invalid free

--- src/unix/proctitle.c
+++ src/unix/proctitle.c
@@ -146,6 +146,8 @@ int uv_get_process_title(char* buffer, size_t size) {
 
 
 UV_DESTRUCTOR(static void free_args_mem(void)) {
-  uv__free(args_mem);  /* Keep valgrind happy. */
-  args_mem = NULL;
+  if (args_mem != NULL) {
+    uv__free(args_mem);  /* Keep valgrind happy. */
+    args_mem = NULL;
+  }
 }
