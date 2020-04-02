66225fc821983c3b5753e1602bdc2251ae8aa284
Lin Zhang <linroid@gmail.com>
2020-04-02 20:38:21 +0800

    android: fix memory overwriting in uv_interface_addresses()

--- src/unix/android-ifaddrs.c
+++ src/unix/android-ifaddrs.c
@@ -476,7 +476,6 @@ static int interpretAddr(struct nlmsghdr *p_hdr, struct ifaddrs **p_resultList, 
                     l_addrSize += NLMSG_ALIGN(calcAddrLen(l_info->ifa_family, l_rtaDataSize));
                     l_addedNetmask = 1;
                 }
-		break;
             case IFA_BROADCAST:
                 l_addrSize += NLMSG_ALIGN(calcAddrLen(l_info->ifa_family, l_rtaDataSize));
                 break;
