f036f1c5e8f08f1af23886fde105c370ad7182ff
Arenoros <arenoros@gmail.com>
2020-05-26 19:41:19 +0300

    Fix build for VS2008

--- src/uv-common.h
+++ src/uv-common.h
@@ -57,13 +57,9 @@ extern int snprintf(char*, size_t, const char*, ...);
 #define container_of(ptr, type, member) \
   ((type *) ((char *) (ptr) - offsetof(type, member)))
 
-
-#if defined(_MSC_VER) && _MSC_VER < 1600
-#define STATIC_ASSERT(expr) // Build error on VS2008
-#else
 #define STATIC_ASSERT(expr)                                                   \
   void uv__static_assert(int static_assert_failed[1 - 2 * !(expr)])
-#endif
+
 /* Handle flags. Some flags are specific to Windows or UNIX. */
 enum {
   /* Used by all handles. */
