f7732e184c5b1c9bf9f67a4135b2dbb5e5b544e5
Momtchil Momtchev <momtchil@momtchev.com>
2020-11-24 19:37:01 +0100

    win: consider a broken pipe a normal EOF

--- src/win/fs.c
+++ src/win/fs.c
@@ -918,7 +918,7 @@ void fs__read(uv_fs_t* req) {
     SET_REQ_RESULT(req, bytes);
   } else {
     error = GetLastError();
-    if (error == ERROR_HANDLE_EOF) {
+    if (error == ERROR_HANDLE_EOF || error == ERROR_BROKEN_PIPE) {
       SET_REQ_RESULT(req, bytes);
     } else {
       SET_REQ_WIN32_ERROR(req, error);
