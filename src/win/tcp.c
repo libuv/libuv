f44a4d0a5f24862388b778fb037408a17db3ff42
theanarkh <2923878201@qq.com>
2020-12-25 01:03:52 +0800

    net:support TCP_KEEPINTVL,TCP_KEEPCNT,TCP_USER_TIMEOUT for tcp keep-alive
    except TCP_KEEPIDLE, linux provide TCP_KEEPINTVL,TCP_KEEPCNT,TCP_USER_TIMEOUT for tcp keep-alive. support this then users have more control over tcp keep-alive.
    
    add two new api uv_tcp_keepalive_ex and uv_tcp_timeout for user to control tcp keepalive

--- src/win/tcp.c
+++ src/win/tcp.c
@@ -1267,7 +1267,6 @@ int uv_tcp_nodelay(uv_tcp_t* handle, int enable) {
   return 0;
 }
 
-
 int uv_tcp_keepalive(uv_tcp_t* handle, int enable, unsigned int delay) {
   int err;
 
@@ -1283,11 +1282,85 @@ int uv_tcp_keepalive(uv_tcp_t* handle, int enable, unsigned int delay) {
     handle->flags &= ~UV_HANDLE_TCP_KEEPALIVE;
   }
 
-  /* TODO: Store delay if handle->socket isn't created yet. */
+  return 0;
+}
+
+int uv_tcp_keepalive_ex(uv_tcp_t* handle,
+                        int enable,
+                        unsigned int delay,
+                        unsigned int interval,
+                        unsigned int count) {
+  int err;
+
+  if (handle->socket != INVALID_SOCKET) {
+    err = uv__tcp_keepalive_ex(handle,
+                               handle->socket,
+                               enable,
+                               delay,
+                               interval,
+                               count);
+    if (err)
+      return err;
+  }
+
+  if (enable) {
+    handle->flags |= UV_HANDLE_TCP_KEEPALIVE;
+  } else {
+    handle->flags &= ~UV_HANDLE_TCP_KEEPALIVE;
+  }
 
   return 0;
 }
 
+static int uv__tcp_keepalive_ex(uv_tcp_t* handle,
+                                SOCKET socket,
+                                int enable,
+                                unsigned int delay,
+                                unsigned int interval,
+                                unsigned int count) {
+  if (setsockopt(socket,
+                 SOL_SOCKET,
+                 SO_KEEPALIVE,
+                 (const char*)&enable,
+                 sizeof enable) == -1) {
+    return WSAGetLastError();
+  }
+
+ #ifdef TCP_KEEPIDLE
+  if (enable && delay && setsockopt(socket,
+                                    IPPROTO_TCP,
+                                    TCP_KEEPIDLE,
+                                    (const char*)&delay,
+                                    sizeof delay) == -1) {
+    return WSAGetLastError();
+  }
+#endif
+#ifdef TCP_KEEPINTVL
+  if (enable && interval && setsockopt(socket,
+                                       IPPROTO_TCP,
+                                       TCP_KEEPINTVL,
+                                       (const char*)&interval,
+                                       sizeof interval) == -1) {
+    return WSAGetLastError();
+  }
+#endif
+#ifdef TCP_KEEPCNT
+  if (enable && count && setsockopt(socket,
+                                    IPPROTO_TCP,
+                                    TCP_KEEPCNT,
+                                    (const char*)&count,
+                                    sizeof count) == -1) {
+    return WSAGetLastError();
+  }
+#endif
+
+  return 0;
+}
+
+/* windows don't support this */
+int uv_tcp_timeout(uv_tcp_t* handle, unsigned int timeout) {
+	return 0;
+}
 
 int uv_tcp_simultaneous_accepts(uv_tcp_t* handle, int enable) {
   if (handle->flags & UV_HANDLE_CONNECTION) {
