e2d0d3d6184d75ab53b1270fbb86dbbf5a4bff01
mikael <mikael@FreeBSD.org>
2021-04-28 16:28:28 +0200

    Fix for ENOTTY error in uv_pipe_open
    Similar to bug https://github.com/libuv/libuv/pull/885

--- src/unix/pipe.c
+++ src/unix/pipe.c
@@ -150,9 +150,13 @@ int uv_pipe_open(uv_pipe_t* handle, uv_file fd) {
   if (mode == -1)
     return UV__ERR(errno); /* according to docs, must be EBADF */
 
+  /* If ioctl(FIONBIO) reports ENOTTY, try fcntl(F_GETFL) + fcntl(F_SETFL).
+   * Workaround for e.g. kqueue fds not supporting ioctls.
+   */
   err = uv__nonblock(fd, 1);
-  if (err)
-    return err;
+  if (err == UV_ENOTTY)
+    if (uv__nonblock == uv__nonblock_ioctl)
+      err = uv__nonblock_fcntl(fd, 1);
 
 #if defined(__APPLE__)
   err = uv__stream_try_select((uv_stream_t*) handle, &fd);
