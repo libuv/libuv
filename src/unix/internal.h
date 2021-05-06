a04801e354b1eaac72819ae3e9e2b62230e11d02
Ali Mohammad Pur <ali.mpfard@gmail.com>
2021-05-06 23:17:00 +0430

    core: Correct the conditionals for {cloexec,nonblock}_ioctl
    These functions are declared with one set of conditionals in the
    header, and defined with another set of conditionals in the c file.
    This commit makes all decisions regarding `uv__{nonblock,cloexec}_ioctl`
    depend on a boolean macro instead.
    There's one function that expects `uv__nonblock_ioctl` to be defined,
    so that bit of the function is also conditionally compiled.

--- src/unix/internal.h
+++ src/unix/internal.h
@@ -176,9 +176,11 @@ struct uv__stream_queued_fds_s {
     defined(__NetBSD__)
 #define uv__cloexec uv__cloexec_ioctl
 #define uv__nonblock uv__nonblock_ioctl
+#define UV__NONBLOCK_IS_IOCTL 1
 #else
 #define uv__cloexec uv__cloexec_fcntl
 #define uv__nonblock uv__nonblock_fcntl
+#define UV__NONBLOCK_IS_IOCTL 0
 #endif
 
 /* On Linux, uv__nonblock_fcntl() and uv__nonblock_ioctl() do not commute
