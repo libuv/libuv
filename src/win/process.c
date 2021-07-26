70325756b498ecee6a36ae8dfc0f08b334ea3e8c
Jameson Nash <vtjnash@gmail.com>
2021-07-26 11:35:57 -0400

    Update src/win/process.c

--- src/win/process.c
+++ src/win/process.c
@@ -171,7 +171,7 @@ static WCHAR* search_path_join_test(const WCHAR* dir,
   DWORD attrs;
   if (dir_len > 2 &&
       ((dir[0] == L'\\' || dir[0] == L'/') &&
-       (dir[1] == L'\\' || dir[1] == L'/')) {
+       (dir[1] == L'\\' || dir[1] == L'/'))) {
     /* It's a UNC path so ignore cwd */
     cwd_len = 0;
   } else if (dir_len >= 1 && (dir[0] == L'/' || dir[0] == L'\\')) {
