9e195958f8fb889268ddf01a7a88fa3b6e4d233f
halx99 <halx99@live.com>
2021-11-08 23:23:08 +0800

    Fix ci

--- src/unix/udp.c
+++ src/unix/udp.c
@@ -665,7 +665,7 @@ int uv__udp_disconnect(uv_udp_t* handle) {
 
     do {
       errno = 0;
-      r = connect(handle->io_watcher.fd, &addr, sizeof(addr));
+      r = connect(handle->io_watcher.fd, (sockaddr*)&addr, sizeof(addr));
     } while (r == -1 && errno == EINTR);
 
     if (r == -1) {
