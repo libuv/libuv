69b811f340f43ba5343eddbe167dd4882e136fe6
Colin Ihrig <cjihrig@gmail.com>
2021-11-19 17:12:04 -0500

    Revert "sunos: Oracle Developer Studio support (#3364)"
    This reverts commit 96b26b1ee291f96d6d5540966118b3599f8375b4.
    
    The commit was reported to break libuv on Illumos systems.

--- include/uv.h
+++ include/uv.h
@@ -43,8 +43,6 @@ extern "C" {
     /* Building static library. */
 #   define UV_EXTERN /* nothing */
 # endif
-#elif defined(__sun)
-# define UV_EXTERN __global
 #elif __GNUC__ >= 4
 # define UV_EXTERN __attribute__((visibility("default")))
 #else
