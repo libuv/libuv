3bb277ae3506d7bf23e8b37a0c52838ff6ea1a0d
Vittore F. Scolari <vittore.scolari@gmail.com>
2022-02-02 14:05:34 +0100

    Use orignial copyright notice for linux-core derived code

--- src/unix/procfs-sysload.c
+++ src/unix/procfs-sysload.c
@@ -1,5 +1,4 @@
-/* Copyright libuv project contributors. All rights reserved.
- *
+/* Copyright Joyent, Inc. and other Node contributors. All rights reserved.
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to
  * deal in the Software without restriction, including without limitation the
