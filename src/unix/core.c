778d631baf2ef9233323870aa06ec4a212bb4df1
Wayne Zhang <shuowang.zhang@ibm.com>
2022-05-05 15:42:05 -0400

    zos: avoid fs event callbacks after uv_close()
    On z/OS, fs events are implemented by registering file interest. When
    closing a fs event handle, it's possible that a change notification has
    already been generated. In that case, the attempt to unregister file
    interest will fail with EALREADY. This will result in the fs event being
    delivered even though the handle is closing, which should not happen.
    
    This resolves https://github.com/libuv/libuv/issues/3601.

--- src/unix/core.c
+++ src/unix/core.c
@@ -160,10 +160,10 @@ void uv_close(uv_handle_t* handle, uv_close_cb close_cb) {
 
   case UV_FS_EVENT:
     uv__fs_event_close((uv_fs_event_t*)handle);
-#if defined(__sun)
+#if defined(__sun) || defined(__MVS__)
     /*
-     * On Solaris and illumos, we will not be able to dissociate the watcher
-     * for an event which is pending delivery, so we cannot always call
+     * On Solaris, illumos, and z/OS we will not be able to dissociate the
+     * watcher for an event which is pending delivery, so we cannot always call
      * uv__make_close_pending() straight away. The backend will call the
      * function once the event has cleared.
      */
