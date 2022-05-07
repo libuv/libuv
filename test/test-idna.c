046e3ef6cb967db094d9f5082d3100ef5c1bf613
ssrlive <30760636+ssrlive@users.noreply.github.com>
2022-05-07 21:15:48 +0800

    C90 and VS 2010 issues.

--- test/test-idna.c
+++ test/test-idna.c
@@ -170,16 +170,16 @@ TEST_IMPL(idna_toascii) {
   T(".ü", ".xn--tda");
   T("ü.ü", "xn--tda.xn--tda");
   T("ü.ü.", "xn--tda.xn--tda.");
-  T("üëäö♥", "xn--4can8av2009b");
+  T("üëäö♥ ", "xn--4can8av2009b");
   T("Willst du die Blüthe des frühen, die Früchte des späteren Jahres",
     "xn--Willst du die Blthe des frhen, "
     "die Frchte des spteren Jahres-x9e96lkal");
   T("ليهمابتكلموشعربي؟", "xn--egbpdaj6bu4bxfgehfvwxn");
-  T("他们为什么不说中文", "xn--ihqwcrb4cv8a8dqg056pqjye");
-  T("他們爲什麽不說中文", "xn--ihqwctvzc91f659drss3x8bo0yb");
+  T("他们为什么不说中文 ", "xn--ihqwcrb4cv8a8dqg056pqjye");
+  T("他們爲什麽不說中文 ", "xn--ihqwctvzc91f659drss3x8bo0yb");
   T("Pročprostěnemluvíčesky", "xn--Proprostnemluvesky-uyb24dma41a");
   T("למההםפשוטלאמדבריםעברית", "xn--4dbcagdahymbxekheh6e0a7fei0b");
-  T("यहलोगहिन्दीक्योंनहींबोलसकतेहैं",
+  T("यहलोगहिन्दीक्योंनहींबोलसकतेहैं ",
     "xn--i1baa7eci9glrd9b2ae1bj0hfcgg6iyaf8o0a1dig0cd");
   T("なぜみんな日本語を話してくれないのか",
     "xn--n8jok5ay5dzabd5bym9f0cm5685rrjetr6pdxa");
@@ -191,15 +191,15 @@ TEST_IMPL(idna_toascii) {
     "xn--PorqunopuedensimplementehablarenEspaol-fmd56a");
   T("TạisaohọkhôngthểchỉnóitiếngViệt",
     "xn--TisaohkhngthchnitingVit-kjcr8268qyxafd2f1b9g");
-  T("3年B組金八先生", "xn--3B-ww4c5e180e575a65lsy2b");
+  T("3年B組金八先生 ", "xn--3B-ww4c5e180e575a65lsy2b");
   T("安室奈美恵-with-SUPER-MONKEYS",
     "xn---with-SUPER-MONKEYS-pc58ag80a8qai00g7n9n");
   T("Hello-Another-Way-それぞれの場所",
     "xn--Hello-Another-Way--fc4qua05auwb3674vfr0b");
   T("ひとつ屋根の下2", "xn--2-u9tlzr9756bt3uc0v");
   T("MajiでKoiする5秒前", "xn--MajiKoi5-783gue6qz075azm5e");
-  T("パフィーdeルンバ", "xn--de-jg4avhby1noc0d");
-  T("そのスピードで", "xn--d9juau41awczczp");
+  T("パフィーdeルンバ ", "xn--de-jg4avhby1noc0d");
+  T("そのスピードで ", "xn--d9juau41awczczp");
   T("-> $1.00 <-", "-> $1.00 <-");
   /* Test cases from https://unicode.org/reports/tr46/ */
   T("faß.de", "xn--fa-hia.de");
