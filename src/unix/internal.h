697ce797f75c9eddd94445e3e01a6502f42dff0f
Ben Noordhuis <info@bnoordhuis.nl>
2022-07-07 10:59:50 +0200

    android: remove pthread-fixes.c
    This was a kludge for a bug in old versions (API level <= 16) of the
    Android SDK.
    
    The os390 port had a build dependency on the file but does not actually
    use it so that too has been removed.

--- src/unix/internal.h
+++ src/unix/internal.h
@@ -79,14 +79,6 @@
 # define UV__PATH_MAX 8192
 #endif
 
-#if defined(__ANDROID__)
-int uv__pthread_sigmask(int how, const sigset_t* set, sigset_t* oset);
-# ifdef pthread_sigmask
-# undef pthread_sigmask
-# endif
-# define pthread_sigmask(how, set, oldset) uv__pthread_sigmask(how, set, oldset)
-#endif
-
 #define ACCESS_ONCE(type, var)                                                \
   (*(volatile type*) &(var))
 
