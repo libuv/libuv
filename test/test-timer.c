1d61e0fc9a3623d5527af8d6f074d1634102878b
Momtchil Momtchev <momtchil@momtchev.com>
2022-10-14 18:49:01 +0200

    this assert was not meant to be removed

--- test/test-timer.c
+++ test/test-timer.c
@@ -308,7 +308,7 @@ TEST_IMPL(timer_run_once) {
   ASSERT(1 == timer_run_once_timer_cb_called);
 
   ASSERT(0 == uv_timer_start(&timer_handle, timer_run_once_timer_cb, 1, 0));
-  uv_run(uv_default_loop(), UV_RUN_ONCE);
+  ASSERT(0 == uv_run(uv_default_loop(), UV_RUN_ONCE));
   ASSERT(2 == timer_run_once_timer_cb_called);
 
   uv_close((uv_handle_t*) &timer_handle, NULL);
