714f6ab6a4c26dc273e092ad2779213aa5f6d06e
Thomas Walter <thomasw@hokla.com>
2024-01-09 17:44:04 +0100

    fix(win): declare empty uv__get_constrained_cpu

--- src/win/util.c
+++ src/win/util.c
@@ -510,6 +510,10 @@ int uv_uptime(double* uptime) {
   return 0;
 }
 
+uv__cpu_constraint uv__get_constrained_cpu(void) {
+  uv__cpu_constraint c = {0, 0, 0};
+  return c;
+}
 
 unsigned int uv_available_parallelism(void) {
   SYSTEM_INFO info;
