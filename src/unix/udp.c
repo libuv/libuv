d6c10a1dc49357baf9b31b27a7af7f8708cd751a
Andy Pan <i@andypan.me>
2024-05-17 17:41:48 +0800

    Fix a typo
    ---------
    
    Signed-off-by: Andy Pan <i@andypan.me>

--- src/unix/udp.c
+++ src/unix/udp.c
@@ -438,7 +438,7 @@ static void uv__udp_sendmsg(uv_udp_t* handle) {
  *
  * Linux as of 3.9 and DragonflyBSD 3.6 have the SO_REUSEPORT socket option but
  * with semantics that are different from the BSDs: it _shares_ the port rather
- * than steal it from the current listener. While useful, it's not something we
+ * than steals it from the current listener. While useful, it's not something we
  * can emulate on other platforms so we don't enable it.
  *
  * zOS does not support getsockname with SO_REUSEPORT option when using
