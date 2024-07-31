1630594db0c4069e48f093c9538d57dc9efafa85
slavamuravey <slavamuravey@mail.ru>
2024-07-31 14:36:39 +0700

    Rollback can_sleep, use it before the loop

--- src/win/core.c
+++ src/win/core.c
@@ -422,9 +422,6 @@ int uv_backend_timeout(const uv_loop_t* loop) {
   return 0;
 }
 
-static int uv__backend_timeout_zero(const uv_loop_t* loop) {
-  return 0;
-}
 
 static void uv__poll_wine(uv_loop_t* loop, DWORD timeout) {
   uv__loop_internal_fields_t* lfields;
@@ -622,7 +619,7 @@ static void uv__poll(uv_loop_t* loop, DWORD timeout) {
 int uv_run(uv_loop_t *loop, uv_run_mode mode) {
   DWORD timeout;
   int r;
-  int (*backend_timeout_fn)(const uv_loop_t* loop);
+  int can_sleep;
 
   r = uv__loop_alive(loop);
   if (!r)
@@ -637,23 +634,16 @@ int uv_run(uv_loop_t *loop, uv_run_mode mode) {
     uv__run_timers(loop);
   }
 
-  backend_timeout_fn = uv__backend_timeout_zero;
-
-  if (
-    mode == UV_RUN_DEFAULT || (
-      mode == UV_RUN_ONCE && 
-      loop->pending_reqs_tail == NULL && 
-      loop->idle_handles == NULL
-  )) {
-    backend_timeout_fn = uv_backend_timeout;
-  }
+  can_sleep = loop->pending_reqs_tail == NULL && loop->idle_handles == NULL;
+  timeout = 0;
 
   while (r != 0 && loop->stop_flag == 0) {
     uv__process_reqs(loop);
     uv__idle_invoke(loop);
     uv__prepare_invoke(loop);
 
-    timeout = backend_timeout_fn(loop);
+    if (mode == UV_RUN_DEFAULT || (mode == UV_RUN_ONCE && can_sleep))
+      timeout = uv_backend_timeout(loop);
 
     uv__metrics_inc_loop_count(loop);
 
