ab23a29328d9ff14f7e3052597f7207942f376cc
Sahilll <150071026+sahilsharda@users.noreply.github.com>
2025-11-26 12:54:26 +0530

    chore: remove addressed TODO in fs-poll.c

--- src/fs-poll.c
+++ src/fs-poll.c
@@ -42,7 +42,7 @@ struct poll_ctx {
   uv_loop_t *loop;
   uv_fs_poll_cb poll_cb;
   uv_timer_t timer_handle;
-  uv_fs_t fs_req; /* TODO(bnoordhuis) mark fs_req internal */
+  uv_fs_t fs_req;
   uv_stat_t statbuf;
   struct poll_ctx *previous; /* context from previous start()..stop() period */
   char path[1];              /* variable length */
