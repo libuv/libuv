2e2114ed8957a0124bc629c61d034c387b8ce42a
Ben Noordhuis <info@bnoordhuis.nl>
2026-01-18 12:21:53 +0100

    unix: fix partial read handling after POLLHUP (#4997)
    It was reported that PTYs on Linux sometimes report POLLHUP, return a
    partial read, but still return more data on the next read.
    
    Libuv contains an optimization where it assumes a partial read after
    POLLHUP means the next read can be skipped because it's going to fail
    with EOF anyway. That assumption was thought to be always true but,
    alas, it isn't.
    
    The fact the optimization has been present for 13 years and this is the
    first bug report about it, indicates how rare this particular condition
    is, but of course we can't skim on correctness.
    
    The reworked optimization only uses POLLHUP as an input signal when
    POLLIN is not also set. That means we no longer have to track partial
    reads because we're going to try and read anyway as long as POLLIN is
    set. It seems to cause no measurable regressions on the test suite or
    the (lightly tested) benchmarks.
    
    Fixes: https://github.com/libuv/libuv/issues/4992

--- test/test-list.h
+++ test/test-list.h
@@ -77,6 +77,7 @@ TEST_DECLARE   (tty_escape_sequence_processing)
 #endif
 TEST_DECLARE   (tty_file)
 TEST_DECLARE   (tty_pty)
+TEST_DECLARE   (tty_pty_partial)
 TEST_DECLARE   (stdio_over_pipes)
 TEST_DECLARE   (stdio_emulate_iocp)
 TEST_DECLARE   (ip6_pton)
@@ -665,6 +666,7 @@ TASK_LIST_START
 #endif
   TEST_ENTRY  (tty_file)
   TEST_ENTRY  (tty_pty)
+  TEST_ENTRY  (tty_pty_partial)
   TEST_ENTRY  (stdio_over_pipes)
   TEST_ENTRY  (stdio_emulate_iocp)
   TEST_ENTRY  (ip6_pton)
